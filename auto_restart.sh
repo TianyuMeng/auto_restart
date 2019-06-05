2c11f75ed6bfa204e2b4b9b90740a9c211c061ad
TianyuMeng <37183405+TianyuMeng@users.noreply.github.com>
2019-06-05 15:33:55 +0800

    Add files via upload

---
+++ auto_restart.sh
@@ -0,0 +1,39 @@
+# ! /bin/sh
+
+SERVICE_PATH="/home/pi/Documents/caiji-intel-fat-client-new/"
+
+SERVICE_NAME="camtest"
+
+START_CMD="./$SERVICE_NAME "
+
+LOG_FILE="restart.log"
+
+
+cd $SERVICE_PATH
+
+pwd
+
+while true 
+
+do
+
+    procnum=`ps -ef|grep $SERVICE_NAME|grep -v grep|wc -l`
+
+    if [ $procnum -eq 0 ]
+
+    then
+
+        echo "start service...................."
+
+        echo `date +%Y-%m-%d` `date +%H:%M:%S`  $SERVICE_NAME >>$LOG_FILE
+
+        ${START_CMD}
+
+    fi
+
+    sleep 1 //每秒检查一轮
+
+done
+
+#https://www.jianshu.com/p/adcddb595786
+#nohup ./auto_restart.sh >/dev/null 2>&1 &
